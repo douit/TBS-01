40f28681b25448abae67e3b8c30221b22559283a
Ahmed Bouzaien <bouzaien_ahmed@yahoio.fr>
2019-11-11 14:52:46 +0300

    Revert datatable footer

--- src/main/webapp/app/shared/datatable/datatable-total-entries/datatable-total-entries.component.ts
+++ src/main/webapp/app/shared/datatable/datatable-total-entries/datatable-total-entries.component.ts
@@ -1,21 +1,13 @@
-import {Component, Input, OnInit} from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-datatable-total-entries',
-  templateUrl: './datatable-total-entries.component.html',
-  styleUrls: ['./datatable-total-entries.component.scss']
+    selector: 'app-datatable-total-entries',
+    templateUrl: './datatable-total-entries.component.html',
+    styleUrls: ['./datatable-total-entries.component.scss']
 })
-export class DatatableTotalEntriesComponent implements OnInit {
-  @Input() rowCount;
-  @Input() offset;
-  @Input() pageSize;
-  @Input() curPage;
-
-  toCal: number;
-
-  ngOnInit() {
-    this.toCal = ((this.curPage * this.pageSize) < this.rowCount) ? (this.curPage * this.pageSize) : this.rowCount;
-    console.log('----'+this.toCal); // object here
-  }
-
+export class DatatableTotalEntriesComponent {
+    @Input() rowCount;
+    @Input() offset;
+    @Input() pageSize;
+    @Input() curPage;
 }
