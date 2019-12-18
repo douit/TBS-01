fd4b311c565d2bfbf8a1bb376b13e6031996c9a4
A.Bouzaien <bouzaien_ahmed@yahoo.fr>
2019-12-18 14:50:35 +0300

    Sadad reconcile sync

--- src/main/java/sa/tamkeentech/tbs/service/RefundService.java
+++ src/main/java/sa/tamkeentech/tbs/service/RefundService.java
@@ -316,8 +316,8 @@ public class RefundService {
      * <p>
      * This is scheduled to get fired every hour, at 01:00 - 02:00 ...
      */
-    // @Scheduled(cron = "0 0 * * * ?")
-    @Scheduled(cron = "0 * * * * ?")
+    @Scheduled(cron = "0 0 * * * ?")
+    // @Scheduled(cron = "0 * * * * ?")
     public void syncSadadRefund() {
         log.info("----Start Sadad Refund Sync");
         boolean successful = true;
